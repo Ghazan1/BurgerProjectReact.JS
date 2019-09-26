3de14a8a95bc7b4b115ea9faf523afaf9e9b9573
Ghazan <meetghz@outlook.com>
2019-09-26 13:23:46 -0700

    remove radium and using method css module

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -1,5 +1,4 @@
 import React,{Component} from 'react';
-import Radium from 'radium';
 
 import './App.css';
 import Person from './Person/Person';
@@ -99,4 +98,4 @@ class App extends Component{
     );
   }
 }
-export default Radium(App);
+export default App;
--- myfirstapp/src/Person/Person.js
+++ myfirstapp/src/Person/Person.js
@@ -11,4 +11,4 @@ const person = (props) => {
     );
 };
 
-export default Radium(person);
+export default person;
